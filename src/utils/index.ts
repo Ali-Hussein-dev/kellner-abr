36bc28068882a2e10f6d089ad511b24991de3ea5
Ali <ali.hussein.pre@gmail.com>
2021-06-18 15:17:05 +0200

    fix(calculated total): fix to 2 decimal places

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export const calcTotal = (obj = { 0: 0 }): number => {
   const values = Object.values(obj);
   const total = values.reduce((acc: number, cur: number) => +cur + acc, 0);
-  return isNaN(total) ? 0 : total;
+  return isNaN(total) ? 0 : +total.toFixed(2);
 };
